8137b4532db15dce0998e693920a707bda03811f
Heather <hdavidson94@gmail.com>
2015-05-23 18:46:20 +0100

    Playing around with the front page
    - There are so many colours in the world

--- front-page.php
+++ front-page.php
@@ -12,9 +12,32 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main main-page" role="main">
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<div id="splash">
+		<h1>HI</h1>
+		<h1>I'M HEATHER</h1>
+	</div>
+	<div id="about">
+		<h2>ABOUT</h2>
+		<div id="about-designer">
+			<h3>DESIGNER</h3>
+		</div>
+		<div id="about-developer">
+			<h3>DEVELOPER</h3>
+		</div>
+		<div id="about-activist">
+			<h3>ACTIVIST</h3>
+			<p>Currently focused on combating toxic, masculinist and inaccesible activism.</p>
+		</div>
+	</div>
+	
+	<div id="contact">
+		<h2>CONTACT</h2>
+		<p>
+			Want to get in touch? You can most often find me on <a href="http://twitter.com/HeatherLauren">Twitter</a>, but 
+			I'm also on <a href="https://github.com/heatherlauren">GitHub</a>. Or, if it's important, you should probably
+			<a href="mailto:hello@heatherlauren.me">send me an email</a>.
+		</p>
+	</div>
+
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -15,23 +15,8 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
-<script src="//use.typekit.net/xfp2fui.js"></script>
-<script>try{Typekit.load();}catch(e){}</script>
 <script src="//use.typekit.net/kvf8llm.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'heatherlauren-theme' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<img class="header--img" src="/wp-content/themes/heatherlauren-theme/imgs/header.svg" alt="">
-		</div><!-- .site-branding -->
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
