e55ecfced850440296ca0737f703c046b187f03e
Heather <hdavidson94@gmail.com>
2015-10-11 15:22:46 +0100

    Sketchbook titles made clickable

--- content-single.php
+++ content-single.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<h1 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 	</header>
 		<div class="entry-meta">
 			<h4>BY <a href="https://twitter.com/HeatherLauren">HEATHER LAUREN</a></h4>
