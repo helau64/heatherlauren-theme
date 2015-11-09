909424be7a983faff9108bec6c870fed25f6191d
Heather <hdavidson94@gmail.com>
2015-11-09 20:12:18 +0000

    Google analytics script added

--- header.php
+++ header.php
@@ -14,8 +14,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/animate.css" type="text/css">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63855327-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/modernizr/modernizr.js"></script>
 <script src="//use.typekit.net/kvf8llm.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
